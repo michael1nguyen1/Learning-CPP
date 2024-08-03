854169deb8b0ef8f9d7857deb9d7a41c9869d73b
michael1nguyen1 <michael22nguyen@gmail.com>
2024-08-03 16:01:59 +0300

    starting ex01

---
+++ cpp01/ex00/include/Zombie.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: linhnguy <linhnguy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 15:50:13 by linhnguy          #+#    #+#             */
+/*   Updated: 2024/08/03 15:56:37 by linhnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+#include <iostream>
+
+class Zombie
+{
+	
+	public:
+
+	void announce( void );
+	
+	private:
+	
+	std::string name;
+};
+
+
+#endif
