7f45af6d88c67ba46852257730fd587cf14b5b84
michael1nguyen1 <michael22nguyen@gmail.com>
2024-08-07 16:57:53 +0300

    cpp01 ex01 done

--- cpp01/ex01/Zombie.cpp
+++ cpp01/ex01/Zombie.cpp
@@ -1,38 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: linhnguy <linhnguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/07 10:54:14 by linhnguy          #+#    #+#             */
-/*   Updated: 2024/08/07 16:56:52 by linhnguy         ###   ########.fr       */
+/*   Created: 2024/08/07 12:02:13 by linhnguy          #+#    #+#             */
+/*   Updated: 2024/08/07 12:03:55 by linhnguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/Zombie.hpp"
 
-void Zombie::announce(void)
+Zombie::Zombie()
 {
-	std::cout << name << ":BraiiiiiiinnnzzzZ..." << std::endl;
-}
-
-Zombie* zombieHorde(int N, std::string name)
-{
-    Zombie  *p = new Zombie[N];
-    
-    for (int i = 0; i < N; i++)
-        p[i] = Zombie(name);
-    return p;
-}
-
-int main()
-{	
-	Zombie *p;
-	int n = 3;
 	
-	p = zombieHorde(n, "James");
-	for (int i = 0; i < n; i++)
-		p[i].announce();
-	delete[](p);
+}
+
+Zombie::Zombie(std::string name) : name(name)
+{
+	
 }
--- cpp01/ex01/include/Zombie.hpp
+++ cpp01/ex01/include/Zombie.hpp
@@ -1,14 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: linhnguy <linhnguy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 12:01:37 by linhnguy          #+#    #+#             */
+/*   Updated: 2024/08/07 12:17:46 by linhnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ZOMBIE_HPP
 #define ZOMBIE_HPP
 
 #include <string>
+#include <iostream>
+#include <stdexcept>
 
 class Zombie
 {
     public:
 
-    Zombie(){};
-    Zombie(std::string name) : name(name){};
+    Zombie();
+    Zombie(std::string name);
+	void announce(void);
     
     private:
 
