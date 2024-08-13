3bd358b24548f9ab6ab97e7272468b828fb9275d
michael1nguyen1 <michael22nguyen@gmail.com>
2024-08-13 22:16:27 +0300

    cpp02 ex00 done

--- cpp02/ex00/include/Fixed.hpp
+++ cpp02/ex00/include/Fixed.hpp
@@ -6,20 +6,29 @@
 /*   By: linhnguy <linhnguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 17:28:46 by linhnguy          #+#    #+#             */
-/*   Updated: 2024/08/13 17:33:35 by linhnguy         ###   ########.fr       */
+/*   Updated: 2024/08/13 22:03:19 by linhnguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_H
 #define FIXED_H
 
+#include <string>
+#include <iostream>
+
 class Fixed
 {
     public:
         Fixed();
-        Fixed(Const Fixed&)();
+        Fixed(const Fixed&);
+        Fixed& operator=(const Fixed&);
+        ~Fixed();
+        int	getRawBits(void);
+        void setRawBits(int const raw);
 
     private:
+        int fixed_point;
+        static const int frac_bits = 8;
 };
 
 
--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -6,7 +6,21 @@
 /*   By: linhnguy <linhnguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 17:28:59 by linhnguy          #+#    #+#             */
-/*   Updated: 2024/08/13 17:29:00 by linhnguy         ###   ########.fr       */
+/*   Updated: 2024/08/13 22:12:10 by linhnguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "include/Fixed.hpp"
+
+int main( void )
+{
+    Fixed a;
+    Fixed b( a ); 
+    Fixed c;
+    
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl; 
+    return 0;
+}
