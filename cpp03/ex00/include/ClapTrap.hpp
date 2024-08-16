66d500a5000dc1c55ceea6491c5f99cba5405ae2
michael1nguyen1 <michael22nguyen@gmail.com>
2024-08-16 13:02:35 +0300

    starting cpp03

---
+++ cpp03/ex00/include/ClapTrap.hpp
@@ -0,0 +1,20 @@
+#ifndef CLAPTRAP_H
+#define CLAPTRAP_H
+
+#include <string>
+
+class ClapTrap
+{
+    public:
+        ClapTrap();
+        ClapTrap(std::string);
+        ClapTrap& operator=(ClapTrap& const);
+        ~ClapTrap();
+
+
+    private:
+
+};
+
+
+#endif
