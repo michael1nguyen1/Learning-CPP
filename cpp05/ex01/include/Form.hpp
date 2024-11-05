7b5a61a729313c0420168cb308a5dd344e85f404
michael1nguyen1 <michael22nguyen@gmail.com>
2024-11-05 12:54:48 +0200

    starting form class

--- cpp05/ex01/include/Form.hpp
+++ cpp05/ex01/include/Form.hpp
@@ -1,13 +1,32 @@
 #ifndef FORM_HPP
 #define FORM_HPP
 
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
 class Form{
 
 	public:
-		
+		Form();
+		Form& Form(Form&);
+
 
 	private:
+		const std::string name;
+		bool signature;
+		const int grade;
+		const int gradeRequired;
 
-}
+	class GradeTooHighException : public std::exception{
+		public:
+			const char *what() const noexcept override;
+	};
+
+	class GradeTooLowException : public std::exception{
+		public:
+			const char *what() const noexcept override;
+	};
+};
 
 #endif
