18a0244b33d6d3f9dd6866554a3c3d979f8fc18e
michael1nguyen1 <michael22nguyen@gmail.com>
2025-01-28 18:30:58 +0200

    ex02 7 almost done

--- cpp07/ex02/include/Array.hpp
+++ cpp07/ex02/include/Array.hpp
@@ -3,11 +3,35 @@
 template<typename T>
 class Array{
     public:
-        Array() : data(nullptr), length(0) {}
+        Array() : data(nullptr), length(0) {std::cout << "create \n";}
         Array(unsigned int n) : length(n){
             data = new T[n]();
         }
+		Array(const Array& other){
+			data = other.data;
+			length = other.length;
+		}
+		Array& operator=(const Array& other){
+			if (this == &other)
+				return *this;
+			delete[] data;
+			data = new T[other.length];
+			for (size_t i = 0; i < other.length; i++)
+				this->data[i] = other.data[i];
+			return *this;
+		}
+		T &operator[](size_t i) {
+			if (i >= length)
+				throw IndexOutOfBoundsException();
+			return data[i];
+		}
+		~Array(){}
+		size_t size() { return length; }
     private:
         T *data;
         size_t length;
+
+	class IndexOutOfBoundsException : public std::exception{
+		const char *what(void) const noexcept { return "Index out of bounds"; };
+	};
 };
--- cpp07/ex02/src/main.cpp
+++ cpp07/ex02/src/main.cpp
@@ -1,19 +1,104 @@
 #include <iostream>
-#include <string>
-#include "Iter.hpp"
+#include <list>
 
-void printint(int i){
-    std::cout << "int is: " << i <<std::endl;
-}
+// void printint(int i){
+//     std::cout << "int is: " << i <<std::endl;
+// }
 
-void printstring(std::string str){
-    std::cout << "String is: " << str <<std::endl;
-}
+// void printstring(std::string str){
+//     std::cout << "String is: " << str <<std::endl;
+// }
 
-int main( void ) {
-    std::string strArray[3] = {"one", "two", "three"};
-    ::iter(strArray, 3, printstring);
+// int main( void ) {
+//     std::string strArray[3] = {"one", "two", "three"};
+//     ::iter(strArray, 3, printstring);
     
-    int array[5] = {1, 2, 3, 4, 5};
-    ::iter(array, 5, printint);
+//     int array[5] = {1, 2, 3, 4, 5};
+//     ::iter(array, 5, printint);
+// }
+int	main(int ac, char **av) {
+	std::list<int>	list;
+
+	for (int i = 0; i < ac; i++)
+		list.push_back(i);
+	for (std::list<int>::iterator i = list.begin(); i != list.end(); ++i)
+		std::cout << *i << "\n";
+	(void)av;
 }
+// {
+// 	try {
+// 		size_t			i;
+// 		Array<int>		a(5);
+// 		Array<char>		b(3);
+// 		Array<float>	c(4);
+// 		Array<double>	d;
+// 		Array<int>		e;
+
+// 		for (i = 0; i < a.size(); i++)
+// 			a[i] = i;
+// 		for (i = 0; i < b.size(); i++)
+// 			b[i] = i + 0x40;
+// 		for (i = 0; i < c.size(); i++)
+// 			c[i] = static_cast<float>(i) * 3.14f;
+
+// 		try {
+// 			for (i = 0;; i++)
+// 				std::cout << "a[" << i << "]: " << a[i] << "\n";
+// 		} catch (std::exception &e) {
+// 			std::cout << e.what() << "\n";
+// 		}
+// 		try {
+// 			for (i = 0;; i++)
+// 				std::cout << "b[" << i << "]: " << b[i] << "\n";
+// 		} catch (std::exception &e) {
+// 			std::cout << e.what() << "\n";
+// 		}
+// 		try {
+// 			for (i = 0;; i++)
+// 				std::cout << "c[" << i << "]: " << c[i] << "\n";
+// 		} catch (std::exception &e) {
+// 			std::cout << e.what() << "\n";
+// 		}
+// 		try {
+// 			for (i = 0;; i++)
+// 				std::cout << "d[" << i << "]: " << d[i] << "\n";
+// 		} catch (std::exception &e) {
+// 			std::cout << e.what() << "\n";
+// 		}
+
+// 		e = a;
+// 		for (i = 0; i < e.size(); i++)
+// 			e[i] *= 3;
+// 		try {
+// 			for (i = 0;; i++)
+// 				std::cout << "a[" << i << "]: " << a[i] << "\n";
+// 		} catch (std::exception &e) {
+// 			std::cout << e.what() << "\n";
+// 		}
+// 		try {
+// 			for (i = 0;; i++)
+// 				std::cout << "e[" << i << "]: " << e[i] << "\n";
+// 		} catch (std::exception &e) {
+// 			std::cout << e.what() << "\n";
+// 		}
+
+// 		Array<int>		f(a);
+// 		try {
+// 			for (i = 0;; i++)
+// 				std::cout << "a[" << i << "]: " << a[i] << "\n";
+// 		} catch (std::exception &e) {
+// 			std::cout << e.what() << "\n";
+// 		}
+// 		try {
+// 			for (i = 0;; i++)
+// 				std::cout << "f[" << i << "]: " << f[i] << "\n";
+// 		} catch (std::exception &e) {
+// 			std::cout << e.what() << "\n";
+// 		}
+
+// 		return 0;
+// 	} catch (std::exception &e) {
+// 		std::cout << "arr: " << e.what() << "\n";
+// 	}
+// 	return 1;
+// }
