be5f0225d21b27edc5ed2815976f5b98c36d50b1
michael1nguyen1 <michael22nguyen@gmail.com>
2025-02-14 17:52:18 +0200

    cpp07 done

--- cpp07/ex02/include/Array.hpp
+++ cpp07/ex02/include/Array.hpp
@@ -4,34 +4,44 @@ template<typename T>
 class Array{
     public:
         Array() : data(nullptr), length(0) {}
+
         Array(unsigned int n) : length(n){
             data = new T[n]();
         }
-		Array(const Array& other){
-			data = other.data;
-			length = other.length;
-		}
-		Array& operator=(const Array& other){
-			if (this == &other)
-				return *this;
-			delete[] data;
+
+		Array(const Array& other) : length(other.length){
 			data = new T[other.length];
 			for (size_t i = 0; i < other.length; i++)
-				this->data[i] = other.data[i];
+				data[i] = other.data[i];
+		}
+
+		Array& operator=(const Array& other){
+			if (this != &other){
+				T* temp = new T[other.length];
+				for (size_t i = 0; i < other.length; i++)
+					temp[i] = other.data[i];
+				delete[] data;
+				data = temp;
+				length = other.length;
+			}
 			return *this;
 		}
+
 		T &operator[](size_t i) {
 			if (i >= length)
 				throw IndexOutOfBoundsException();
 			return data[i];
 		}
-		~Array(){}
-		size_t getLength() { return length; }
+		~Array(){ delete [] data;}
+
+		size_t getLength() const { return length; }
+   
+		class IndexOutOfBoundsException : public std::exception{
+			const char *what(void) const noexcept { return "Index out of bounds"; };
+		};
+
     private:
         T *data;
         size_t length;
 
-	class IndexOutOfBoundsException : public std::exception{
-		const char *what(void) const noexcept { return "Index out of bounds"; };
-	};
 };
--- cpp07/ex02/src/main.cpp
+++ cpp07/ex02/src/main.cpp
@@ -5,77 +5,38 @@
 int	main() {
 	try {
 		size_t			i;
-		Array<int>		a(5);
-		Array<char>		b(3);
-		Array<float>	c(4);
-		Array<double>	d;
-		Array<int>		e;
+		Array<int>		intArray(5);
+		Array<char>		charArray(3);
 
-		for (i = 0; i < a.getLength(); i++)
-			a[i] = i;
-		for (i = 0; i < b.getLength(); i++)
-			b[i] = i + 0x40;
-		for (i = 0; i < c.getLength(); i++)
-			c[i] = static_cast<float>(i) * 3.14f;
+		for (i = 0; i < intArray.getLength(); i++)
+			intArray[i] = i;
+		for (i = 0; i < charArray.getLength(); i++)
+			charArray[i] = i + 33;
 
 		try {
 			for (i = 0;; i++)
-				std::cout << "a[" << i << "]: " << a[i] << "\n";
+				std::cout << "intArray[" << i << "]: " << intArray[i] << "\n";
 		} catch (std::exception &e) {
 			std::cout << e.what() << "\n";
 		}
 		try {
 			for (i = 0;; i++)
-				std::cout << "b[" << i << "]: " << b[i] << "\n";
-		} catch (std::exception &e) {
-			std::cout << e.what() << "\n";
-		}
-		try {
-			for (i = 0;; i++)
-				std::cout << "c[" << i << "]: " << c[i] << "\n";
-		} catch (std::exception &e) {
-			std::cout << e.what() << "\n";
-		}
-		try {
-			for (i = 0;; i++)
-				std::cout << "d[" << i << "]: " << d[i] << "\n";
+				std::cout << "charArray[" << i << "]: " << charArray[i] << "\n";
 		} catch (std::exception &e) {
 			std::cout << e.what() << "\n";
 		}
 
-		e = a;
-		for (i = 0; i < e.getLength(); i++)
-			e[i] *= 3;
-		try {
-			for (i = 0;; i++)
-				std::cout << "a[" << i << "]: " << a[i] << "\n";
-		} catch (std::exception &e) {
-			std::cout << e.what() << "\n";
-		}
-		try {
-			for (i = 0;; i++)
-				std::cout << "e[" << i << "]: " << e[i] << "\n";
-		} catch (std::exception &e) {
-			std::cout << e.what() << "\n";
-		}
-
-		Array<int>		f(a);
-		try {
-			for (i = 0;; i++)
-				std::cout << "a[" << i << "]: " << a[i] << "\n";
-		} catch (std::exception &e) {
-			std::cout << e.what() << "\n";
-		}
+		Array<int>f(intArray); //copy
 		try {
 			for (i = 0;; i++)
 				std::cout << "f[" << i << "]: " << f[i] << "\n";
 		} catch (std::exception &e) {
 			std::cout << e.what() << "\n";
 		}
-
 		return 0;
+
 	} catch (std::exception &e) {
-		std::cout << "arr: " << e.what() << "\n";
+		std::cout << "New failed: " << e.what() << "\n";
 	}
 	return 1;
 }
